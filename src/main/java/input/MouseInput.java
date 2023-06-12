73130ff917f590818f2bad11c8b6bb8266971f54
TreNgheDiCode <gabayan170@gmail.com>
2023-06-13 01:15:59 +0700

    Episode 13 - Adding Sound, SFX Button

--- src/main/java/input/MouseInput.java
+++ src/main/java/input/MouseInput.java
@@ -1,16 +1,38 @@
 package input;
 
-import main.GamePanel;
-import state.Gamestate;
-
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
+import state.Gamestate;
+import main.GamePanel;
+
 public class MouseInput implements MouseListener, MouseMotionListener {
+
     private GamePanel gamePanel;
+
     public MouseInput(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        switch (Gamestate.state) {
+            case MENU:
+                gamePanel.getGame().getMenu().mouseMoved(e);
+                break;
+            case PLAYING:
+                gamePanel.getGame().getPlaying().mouseMoved(e);
+                break;
+            default:
+                break;
+
+        }
+
     }
 
     @Override
@@ -19,40 +41,48 @@ public class MouseInput implements MouseListener, MouseMotionListener {
             case PLAYING:
                 gamePanel.getGame().getPlaying().mouseClicked(e);
                 break;
-            case MENU:
-                gamePanel.getGame().getMenu().mouseClicked(e);
-                break;
-            case OPTIONS:
-                break;
-            case QUIT:
-                break;
             default:
                 break;
+
         }
+
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
         switch (Gamestate.state) {
-            case PLAYING -> gamePanel.getGame().getPlaying().mousePressed(e);
-            case MENU -> gamePanel.getGame().getMenu().mousePressed(e);
-            default -> {
-            }
+            case MENU:
+                gamePanel.getGame().getMenu().mousePressed(e);
+                break;
+            case PLAYING:
+                gamePanel.getGame().getPlaying().mousePressed(e);
+                break;
+            default:
+                break;
+
         }
+
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         switch (Gamestate.state) {
-            case PLAYING -> gamePanel.getGame().getPlaying().mouseReleased(e);
-            case MENU -> gamePanel.getGame().getMenu().mouseReleased(e);
-            default -> {
-            }
+            case MENU:
+                gamePanel.getGame().getMenu().mouseReleased(e);
+                break;
+            case PLAYING:
+                gamePanel.getGame().getPlaying().mouseReleased(e);
+                break;
+            default:
+                break;
+
         }
+
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
 
     }
 
@@ -61,18 +91,4 @@ public class MouseInput implements MouseListener, MouseMotionListener {
 
     }
 
-    @Override
-    public void mouseDragged(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        switch (Gamestate.state) {
-            case PLAYING -> gamePanel.getGame().getPlaying().mouseMoved(e);
-            case MENU -> gamePanel.getGame().getMenu().mouseMoved(e);
-            default -> {
-            }
-        }
-    }
 }
